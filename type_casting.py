ab1ed5f7b80b82a464ac404a5fe696440cb298b5
Hashir <hashirbutt1996@gmail.com>
2026-08-26 13:09:14 +0500

    type castig in python

--- type_casting.py
+++ type_casting.py
@@ -2,3 +2,8 @@ age = input("Enter your age: ")
 
 newAge = int(age) + 1
 print(newAge)
+
+# Below code will give an error becasue we cant convert alphabets into integer
+alphabaticals = "ABCD"
+testing  = int(alphabaticals)
+print(testing)
