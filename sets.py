e1392c99698cb79e2d28cf705ba56d0b8115459e
Hashir <hashirbutt1996@gmail.com>
2026-08-26 16:31:10 +0500

    learned sets

---
+++ sets.py
@@ -0,0 +1,8 @@
+# set => unique item collection
+marks = {1,2,3,9,4,5,6,1,7,8,1,0}
+
+print(len(marks)) # => will return length according to the unique digits
+print(marks) # => {0,1,2,3,4,5,6,7,8,9}
+
+alpha = {'a','c','d','f','i','g', 'b'}
+print(alpha)
