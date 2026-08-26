c8048515df6036afeedd779dde8a8fec9af9dc7f
Hashir <hashirbutt1996@gmail.com>
2026-08-26 13:15:43 +0500

    type conversion

--- type_casting.py
+++ type_casting.py
@@ -1,9 +1,13 @@
+# Type casting
 age = input("Enter your age: ")
 
 newAge = int(age) + 1
 print(newAge)
 
 # Below code will give an error becasue we cant convert alphabets into integer
-alphabaticals = "ABCD"
-testing  = int(alphabaticals)
-print(testing)
+#alphabaticals = "ABCD"
+#testing  = int(alphabaticals)
+#print(testing)
+
+#Type conversion
+print(1 + True) # => 2
